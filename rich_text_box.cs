4fe50c082da41bd8dc5d506083cdf4fd24eac61d
matijaopacak06 <153612293+matijaopacak06@users.noreply.github.com>
2024-02-13 12:08:53 +0100

    Add files via upload

---
+++ rich_text_box.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rich_text_box
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SaveFile(@"C:Tempproba.rtf");
+
+            MessageBox.Show("Tekst spremljen");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.LoadFile(@"C:Tempproba.rtf");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+    }
+}
